93dfedd7952a9b53111ee3d7c8f0d5d4c0514048
mzokov <mzokov@users.noreply.github.com>
2019-04-01 18:46:57 +0100

    Fixed React app

--- base-react-app/.next/static/runtime/webpack.js
+++ base-react-app/.next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5ba5bffb6c7c6ad99422";
+/******/ 	var hotCurrentHash = "ce21eb391298757f89a4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
