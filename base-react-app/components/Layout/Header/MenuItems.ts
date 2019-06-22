75ad4050f9c1a9e172ab9bbf1935628e87e74ed1
martinzokov <m.zokov@abv.bg>
2019-06-22 18:06:59 +0100

    Remove frontend project

--- base-react-app/components/Layout/Header/MenuItems.ts
+++
@@ -1,8 +0,0 @@
-export default class MenuItem{
-    label: string;
-    path: string;
-    constructor(label: string, path: string){
-        this.label = label;
-        this.path = path;
-    }
-}
--- base-react-app/components/Layout/index.ts
+++
@@ -1,3 +0,0 @@
-
-export {Header} from './Header/Header';
-export {Footer} from './Footer/Footer';
